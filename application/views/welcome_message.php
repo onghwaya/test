9844e52266d302579814858f5e6730dc47be5f3e
onghwa <onghwaya@gmail.com>
2014-05-14 18:43:46 +0900

    chmod

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -78,7 +78,7 @@
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/welcome.php</code>
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">유저가이드</a>.</p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
